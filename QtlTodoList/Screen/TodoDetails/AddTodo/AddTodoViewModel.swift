e1bfc9e978967ab90472b81b0238efdf8edc1f05
kakizaki hayate <hayate.h.0704@gmail.com>
2023-07-09 14:38:15 +0900

    AddTodoViewModelにカメラかライブラリを起動する関数を追加

--- QtlTodoList/Screen/TodoDetails/AddTodo/AddTodoViewModel.swift
+++ QtlTodoList/Screen/TodoDetails/AddTodo/AddTodoViewModel.swift
@@ -5,13 +5,30 @@
 //  Created by 柿崎逸 on 2023/06/24.
 //
 
-import Foundation
+import SwiftUI
 
 class AddTodoViewModel: ObservableObject {
     // MARK: - Property Wrappers
     @Published var title = ""
     @Published var message = ""
     @Published var isTextEmpty = false
-    // MARK: - Properties
-    static let shared = AddTodoViewModel()
+    @Published var isCamera = false
+    @Published var isLibrary = false
+    @Published var image = UIImage()
+    @Published var imageURL: String?
+}
+
+extension AddTodoViewModel {
+    // MARK: - Methods
+    func selectedImagePicker(selectedImageSource: Int) {
+        let value = ImageSource(rawValue: selectedImageSource)
+        switch value {
+        case .camera:
+            self.isCamera.toggle()
+        case .Library:
+            self.isLibrary.toggle()
+        default:
+            break
+        }
+    }
 }
